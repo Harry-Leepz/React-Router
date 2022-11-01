d0b0ccd3c367bee43e1da0058d97fb5da70ee75d
Harry Dhillon <hdhillon478@gmail.com>
2022-11-01 21:15:03 +0000

    Add Links to Header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Route } from "react-router-dom";
 
 import Welcome from "./components/Welcome";
 import Products from "./components/Products";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Route path='/welcome'>
         <Welcome />
       </Route>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <nav>
         <ul>
-          <li></li>
+          <li>
+            <Link to='/welcome'>Welcome</Link>
+          </li>
+          <li>
+            <Link to='/products'>Products</Link>
+          </li>
         </ul>
       </nav>
     </header>
