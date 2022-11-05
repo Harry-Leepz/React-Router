bab17b446eefedac70357215f5f52520e1edf88d
Harry Dhillon <hdhillon478@gmail.com>
2022-11-05 20:22:45 +0000

    Add basic ul with some dummy list items

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,6 +4,11 @@ const Products = () => {
   return (
     <>
       <h1>Welcome to Products Page!</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
     </>
   );
 };
